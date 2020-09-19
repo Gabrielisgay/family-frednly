7e030d6891cdd35225185d4e06a0f26c6b48b6ac
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-19 20:57:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
  
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const commandName = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
@@ -25,10 +25,11 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
+	if (!client.commands.has(commandName)) return;
+	const command = client.commands.get(commandName)
 
 	try {
-		client.commands.get(command).execute(message, args);
+		command.execute(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
