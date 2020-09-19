593402a840fc753be0560856dca5a0165e8514cc
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-19 21:05:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(' ');
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
 	if (!client.commands.has(commandName)) return;
 	const command = client.commands.get(commandName)
