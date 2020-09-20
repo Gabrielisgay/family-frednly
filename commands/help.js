d9e557517a70e2cf90c3cef41012438629bd280e
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-20 16:58:13 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -21,6 +21,7 @@ module.exports = {
 		      console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
 		      message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
 	      });
+   }
    const name = args[0].toLowerCase();
    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
@@ -37,6 +38,5 @@ module.exports = {
    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
    message.channel.send(data, { split: true });
-   }
   }
 }
