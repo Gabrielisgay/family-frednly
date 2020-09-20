0657b18f840d295b2bac111d214022b6f5dfa5cb
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-20 17:09:50 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -29,11 +29,10 @@ module.exports = {
    return message.reply('that\'s not a valid command!');
    }
 
-   data.push(`**Name:** ${command.name}`);
-
-   if (command.aliases) data.push(````**Aliases:** ${command.aliases.join(', ')}````);
-   if (command.description) data.push(````**Description:** ${command.description}````);
-   if (command.usage) data.push(````**Usage:** ${prefix}${command.name} ${command.usage}````);
+   data.push(`**Name:** ${command.name}`)
+   if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+   if (command.description) data.push(`**Description:** ${command.description}`);
+   if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
