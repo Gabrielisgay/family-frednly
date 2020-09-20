41231cd6add42dbb71356fd98b2f4fab1c37fd4f
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-20 12:38:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,4 +38,4 @@ client.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
-client.user.setActivity("pc help",{type: "playing"});
+.then(client.user.setActivity("pc help",{type: "playing"}));
