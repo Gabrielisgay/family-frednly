9242953d297a1f5078dfbed96beeed777e1b6a45
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-20 16:19:14 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,8 @@ const { prefix } = require('../config.json')
 module.exports = {
   name: 'help',
   description: 'help!',
+  aliases: ['commands'],
+  usage: '[command name]',
   execute(message, args) {
     const data = [];
     const { commands } = message.client;
